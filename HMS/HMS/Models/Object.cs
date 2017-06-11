50ff92d9076e9608869518e5336c1d62a51011ee
Yunus <guesswhat.1@web.de>
2017-06-11 11:15:02 +0200

    Bugfix Konstruktor + base() == super() konstruktor room, user, patient, person etc.

--- HMS/HMS/Models/Object.cs
+++ HMS/HMS/Models/Object.cs
@@ -11,13 +11,26 @@ namespace HMS.Models
 {
     using System;
     using System.Collections.Generic;
-    
+
     public partial class Object
     {
         public int Id { get; set; }
         public System.DateTime timecreate { get; set; }
         public System.DateTime timemodify { get; set; }
         public bool isactive { get; set; }
+
+        public Object()
+        {
+
+        }
+
+        public Object(int id, System.DateTime timecreate, System.DateTime timemodify, bool isactive)
+        {
+            this.Id = id;
+            this.timecreate = timecreate;
+            this.timemodify = timemodify;
+            this.isactive = isactive;
+        }
     }
 
 
