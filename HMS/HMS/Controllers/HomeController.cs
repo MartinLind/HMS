64b15371c32f4177ad937866ce237dfdac108a41
guangming01 <guangming.yang@outlook.com>
2017-06-29 11:40:02 +0200

    5 right return 5 different view

--- HMS/HMS/Controllers/HomeController.cs
+++ HMS/HMS/Controllers/HomeController.cs
@@ -40,8 +40,17 @@ namespace HMS.Controllers
 
             switch (FunctionLogin(username, password))
             {
-                case FunctionLoginStatus.SUCCESS:
+                case FunctionLoginStatus.SUCCESS1:
                     result = View("Home", "_Layout1");
+                    break;
+                case FunctionLoginStatus.SUCCESS2:
+                    result = View("Home", "_Layout2");
+                    break;
+                case FunctionLoginStatus.SUCCESS3:
+                    result = View("Home", "_Layout3");
+                    break;
+                case FunctionLoginStatus.SUCCESS4:
+                    result = View("Home", "_Layout4");
                     break;
                 case FunctionLoginStatus.FAIL:
                     ViewBag.LoginMessage = "Something wrong! Try Again!";
@@ -61,7 +70,7 @@ namespace HMS.Controllers
 
         public enum FunctionLoginStatus
         {
-            SUCCESS,
+            SUCCESS1, SUCCESS2, SUCCESS3, SUCCESS4,
             FAIL,
             FIRED,
             RETIRE
@@ -84,16 +93,22 @@ namespace HMS.Controllers
             SqlDataReader reader = cmd.ExecuteReader();
 
             String dbUsername = null;
+            Boolean dbright1 = false;
+            Boolean dbright2 = false;
+            Boolean dbright3 = false;
+            Boolean dbright4 = false;
             Boolean dbstatus = false;
 
             while (reader.Read() == true)
             {
                 dbUsername = Convert.ToString(reader["username"]);
                 /// ......
+                dbright1 = Convert.ToBoolean(reader["accessright1"]);
+                dbright2 = Convert.ToBoolean(reader["accessright2"]);
+                dbright3 = Convert.ToBoolean(reader["accessright3"]);
+                dbright4 = Convert.ToBoolean(reader["accessright4"]);
                 dbstatus = Convert.ToBoolean(reader["accessright5"]);
-                /// .......
-                /// ......
-                /// ......
+                // so far use Recht5 instead status
             }
 
             if (dbUsername == null)
@@ -104,11 +119,27 @@ namespace HMS.Controllers
             else
             {
                 /// success
-                if (dbstatus == true) // so far use Recht5 instead status
+                if (dbright1 == true && dbstatus == true ) 
                 {
                     /// success , and status==true
-                    return FunctionLoginStatus.SUCCESS;
+                    return FunctionLoginStatus.SUCCESS1;
                 }
+                if (dbright2 == true && dbstatus == true)
+                {
+                    /// success , and status==true
+                    return FunctionLoginStatus.SUCCESS2;
+                }
+                if (dbright3 == true && dbstatus == true)
+                {
+                    /// success , and status==true
+                    return FunctionLoginStatus.SUCCESS3;
+                }
+                if (dbright4 == true && dbstatus == true)
+                {
+                    /// success , and status==true
+                    return FunctionLoginStatus.SUCCESS4;
+                }
+               
                 else
                 {
                     /// success , and status==false
